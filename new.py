2d63e83817815a7b67e83ec3fef50210571ffb83
dam <0304mshungo@gmail.com>
2025-03-29 22:42:48 +0900

    cont

---
+++ new.py
@@ -0,0 +1,7 @@
+a=[1,3,4,7,9]
+b=(1,3,4,7,9)
+
+if a==b:
+    print("hitosii")
+else:
+    print("hitosikunai")
--- same2.py
+++ same2.py
@@ -5,32 +5,30 @@ map=[
 g=int(input())
 r=int(input())
 stk=[]
+stk2=[]
 pas=[]
-ct=0
 while True:
     
     pas.append((g,r))
-    if g<2 and map[g+1][r]==map[g][r] and [g+1,r] not in pas:
+    if g<2 and map[g+1][r]==map[g][r] and (g+1,r) not in pas:
         stk.append((g+1,r))
-        pas.append((g+1,r))
-    if g>0 and map[g-1][r]==map[g][r] and [g-1,r] not in pas:
+
+    if g>0 and map[g-1][r]==map[g][r] and (g-1,r) not in pas:
         stk.append((g-1,r))
-        pas.append((g-1,r))
-    if r<3 and map[g][r+1]==map[g][r] and [g,r+1] not in pas:
+
+    if r<3 and map[g][r+1]==map[g][r] and (g,r+1) not in pas:
         stk.append((g,r+1))
-        pas.append((g,r+1))
-    if r>0 and map[g][r-1]==map[g][r] and [g,r-1] not in pas:
+        
+    if r>0 and map[g][r-1]==map[g][r] and (g,r-1) not in pas:
         stk.append((g,r-1))
-        pas.append((g,r-1))
+        
+    print("g=",g)
+    print("r=",r)
     print("stk=",stk)
+    print("stk2=",stk2)
     print("pas=",pas)
-    g=stk[0][0]
-    r=stk[0][1]
     if stk==[]:
         break
-    ct=ct+1
-    if ct==10:
-        break    #テスト用テスト用
-
+    g,r=stk.pop(0)
 
     
